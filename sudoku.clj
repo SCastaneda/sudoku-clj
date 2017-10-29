6fd38ce08c956f0970ed2e8868a680177da67eac
Samuel Castaneda <sam@sam-the-man.com>
2017-10-29 21:15:38 +0200

    clean up

--- sudoku.clj
+++ sudoku.clj
@@ -1,6 +1,8 @@
+(ns sudoku)
+
 (use 'clojure.java.io)
 
-(defn in? [coll elm]  
+(defn in? [coll elm]
   (some #(= elm %) coll)
   )
 
@@ -8,8 +10,8 @@
   (nth (nth board x) y)
   )
 
-(defn is-valid? 
-  "Checks if the area is valid. 
+(defn is-valid?
+  "Checks if the area is valid.
   Area is a list, transformed from a row, column, or square.
   The list is first stripped of zeros, then checked for duplicates.
   Checking for duplicates is done by sorting the list and checking for equality
@@ -18,10 +20,9 @@
   (let [sorted (sort (filter #(not (zero? %)) area))]
     ((fn [l]
       (let [n (count l)]
-        (cond 
+        (cond
           (<= n 1) true
-          (= n 2) (not (= (first l) (second l)))
-          (> n 2) (if (= (first l) (second l)) 
+          (>= n 2) (if (= (first l) (second l))
                       false
                       (recur (rest l)))
           )
@@ -30,25 +31,25 @@
     )
   )
 
-(defn get-square-coordinates 
-  "Given one fields coordinates in a board, 
-  returns the coordinates of entire square on the board
+(defn get-square-coordinates
+  "Given one field's coordinates on a board,
+  returns the coordinates of entire square on the board.
   board is assumed to be a 9x9 and a square is a 3x3"
-  [x y] 
-  (let [begx (* (quot x 3) 3) 
-        endx (+ begx 3) 
-        begy (* (quot y 3) 3) 
-        endy (+ begy 3)] 
-    (for [row (range begx endx) col (range begy endy)] 
+  [x y]
+  (let [begx (* (quot x 3) 3)
+        endx (+ begx 3)
+        begy (* (quot y 3) 3)
+        endy (+ begy 3)]
+    (for [row (range begx endx) col (range begy endy)]
           [row col]
-      ) 
+      )
     )
   )
 
 (defn get-square [board x y]
-  (let [fields (get-square-coordinates x y)] 
-    (into [] 
-      (map #(get-field board (first %) (second %)) 
+  (let [fields (get-square-coordinates x y)]
+    (into []
+      (map #(get-field board (first %) (second %))
         fields
         )
       )
@@ -63,11 +64,15 @@
   (into [] (map #(nth % col-num) board))
   )
 
-(defn get-all-possible [board x y] 
-  (let [poss (range 1 10)] 
-    (filter #(and 
-                (not (in? (get-row board x) %)) 
-                (not (in? (get-col board y) %)) 
+(defn get-all-possible [board x y]
+  "given the current board state, get all possible
+  numbers at the given coordinates(x, y).
+  This is simply done by filtering all existing numbers
+  of this coordinates row, col, and square."
+  (let [poss (range 1 10)]
+    (filter #(and
+                (not (in? (get-row board x) %))
+                (not (in? (get-col board y) %))
                 (not (in? (get-square board x y) %))
               )
         poss
@@ -76,35 +81,36 @@
   )
 
 (defn is-solvable? [board]
-  (let [rows-n-cols 
-    ((fn [bd n]
-    (cond
-      (< n 0) true
-      (and  (is-valid? (get-row bd n))
-            (is-valid? (get-col bd n))
-            ) (recur bd (- n 1))
-      true false
-            
-      )
-    ) board (- (count board) 1))
-
-    squares
-    ((fn [bd squares]
-      (cond
-        (empty? squares) true
-        true 
-          (let [square (first squares) x (first square) y (second square)]
-            (if (is-valid? (get-square bd x y))
-                (recur bd (rest squares))
-                false
-              )
-            )
-        
+  (let [
+    check-rows-n-cols
+      (fn [bd n]
+        (cond
+          (< n 0) true
+          (and  (is-valid? (get-row bd n))
+                (is-valid? (get-col bd n))
+                ) (recur bd (- n 1))
+          :else false
+          )
         )
-      ) board (for [x (range 3) y (range 3)] [(* x 3) (* y 3)]))
 
+    check-squares
+      (fn [bd squares]
+        (cond
+          (empty? squares) true
+          :else
+            (let [square (first squares) x (first square) y (second square)]
+              (if (is-valid? (get-square bd x y))
+                  (recur bd (rest squares))
+                  false
+                )
+              )
+          )
+        )
     ]
-    (and rows-n-cols squares)
+    (and
+      (check-rows-n-cols board (- (count board) 1))
+      (check-squares board (for [x (range 3) y (range 3)] [(* x 3) (* y 3)]))
+      )
     )
   )
 
@@ -138,8 +144,8 @@
 
 
 
-(defn is-valid? 
-  "Checks if the area is valid. 
+(defn is-valid?
+  "Checks if the area is valid.
   Area is a list, transformed from a row, column, or square.
   The list is first stripped of zeros, then checked for duplicates.
   Checking for duplicates is done by sorting the list and checking for equality
@@ -148,10 +154,10 @@
   (let [sorted (sort (filter #(not (zero? %)) area))]
     ((fn [l]
       (let [n (count l)]
-        (cond 
+        (cond
           (<= n 1) true
           (= n 2) (not (= (first l) (second l)))
-          (> n 2) (if (= (first l) (second l)) 
+          (> n 2) (if (= (first l) (second l))
                       false
                       (recur (rest l)))
           )
@@ -160,39 +166,38 @@
     )
   )
 
-(defn attempt 
+(defn attempt
   ([board]
     (attempt board 0 0))
   ([board x y]
     (if (= 0 (get-field board x y))
-        ;; get-all-possible
         (let [poss (get-all-possible board x y)]
           (attempt board x y poss)
           )
-        (cond 
+        (cond
           (and (= x 8) (= y 8)) board ;; we're done
-          true (if (< y 8) 
-                (attempt board x (inc y))
-                (attempt board (inc x) 0)
-                )
+          :else (if (< y 8)
+                  (attempt board x (inc y))
+                  (attempt board (inc x) 0)
+                  )
           )
       )
     )
   ([board x y poss]
     (cond
-      (empty? poss) false
-      (and (= x 8) (= y 8)) (assoc-in board [x y] (first poss)) 
-      true 
+      (empty? poss) false ;; return false to start backtracking
+      (and (= x 8) (= y 8)) (assoc-in board [x y] (first poss))
+      :else
         (let [att (assoc-in board [x y] (first poss))
-              p (if (< y 8) 
+              p (if (< y 8)
                       (attempt att x (inc y))
                       (attempt att (inc x) 0)
                       )
               ]
-              (if-not p 
+              (if-not p ;; returnd false... try the next possible number
                       (attempt board x y (rest poss))
-                      p 
-                )    
+                      p ;; returned a board... keep sending it back up
+                )
           )
       )
     )
@@ -202,12 +207,17 @@
   (get-board (split-by-nl (slurp filename)))
   )
 
-(defn solve [filename]
-  (let [board (get-board-from-file filename)]
-    (if (is-solvable? board)
-        (print-board (attempt board))
-        (println "Board not solvable")
-      )
-    
+(defn solve [board]
+  (if (is-solvable? board)
+      (print-board (attempt board))
+      (println "Board not solvable")
     )
   )
+
+(defn solve-file[filename]
+  (let [board (get-board-from-file filename)]
+    (solve board)
+    )
+  )
+
+;;(solve-file "/Users/sam/projects/clojure/sudoku.txt")
